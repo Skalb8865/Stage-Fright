2d51e24e4442db318a7b05d5327458bff4ac49b2
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2024-12-11 02:52:05 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,123 @@
+// SHOW MENU =========================
+const navMenu = document.getElementById("main-nav");
+const navOpen = document.getElementById("nav-open");
+const navClose = document.getElementById("nav-close");
+
+/*===== SHOW MENU =====*/
+navOpen.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+});
+
+/*===== HIDE MENU =====*/
+navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+});
+
+// REMOVE MENU ON CLICK FOR MOBILE =========================
+const navLink = document.querySelectorAll(".nav-link");
+
+function linkAction() {
+    const navMenu = document.getElementById("main-nav");
+
+    navMenu.classList.remove("show-menu");
+}
+navLink.forEach((link) => link.addEventListener("click", linkAction));
+
+
+// CHANGE THE BACKGROUND ON SCROLL =========================
+function changeHeaderColor() {
+    const nav = document.getElementById("header");
+
+    if (this.scrollY >= 200) nav.classList.add("scroll-header");
+    else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", changeHeaderColor);
+
+/*--------------------------------------album1,2,3 js--------------------------------*/
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+const navLogo = document.querySelector('#navbar_logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+    console.log(scrollPos);
+
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight');
+    }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+/*--------------------------------------END OF album1,2,3 js--------------------------------*/
+/*-------------------------------------Start of merch pages------------------------------*/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+/*--------------------------------------END OF merch page--------------------------------*/
+
+
