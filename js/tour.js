b78d2246276df261032761582009934d46b5cb29
Skalb8865 <153946850+Skalb8865@users.noreply.github.com>
2025-01-13 19:53:29 -0500

    .

--- js/tour.js
+++ js/tour.js
@@ -18,3 +18,25 @@ dialog.addEventListener('click', event => {
     dialog.close();
   }
 });
+
+
+const open2Btn = document.querySelector('#open-2-modal');
+const dialog2 = document.querySelector('#dialog2');
+const close2Btn = document.querySelector('#close-2-modal');
+
+open2Btn.addEventListener('click', () => dialog2.showModal());
+close2Btn.addEventListener('click', () => dialog2.close());
+
+// close modal when clicking outside
+dialog2.addEventListener('click', event => {
+  const rect = dialog2.getBoundingClientRect();
+  const isIndialog2 =
+    event.clientX >= rect.left &&
+    event.clientX <= rect.right &&
+    event.clientY >= rect.top &&
+    event.clientY <= rect.bottom;
+
+  if (!isIndialog2) {
+    dialog2.close();
+  }
+});
